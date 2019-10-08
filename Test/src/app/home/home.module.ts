506d3634357d76dc3476536e229e97aae5980f98
59 Dev <59.dev.solutions@gmail.com>
2019-10-08 15:14:44 -0500

    add module to home module per instructions

--- Test/src/app/home/home.module.ts
+++ Test/src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-
+import { DropDownModule } from "nativescript-drop-down/angular";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        DropDownModule
     ],
     declarations: [
         HomeComponent
